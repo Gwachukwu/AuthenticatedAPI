45449b81cb3d6fb717409326b2b1a4cb97a2d84c
gwachukwu <gwachukwuelebeke@gmail.com>
2024-02-22 22:57:19 -0500

    Add identity routes

--- ShopAPI/Program.cs
+++ ShopAPI/Program.cs
@@ -13,12 +13,19 @@ builder.Services.AddDbContext<AppSecurityContext>(
     options => options.UseInMemoryDatabase("SecurityDB")
 );
 
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AppSecurityContext>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
